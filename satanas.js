b5f98bbeea4f6ca1831b3028bd83a77eb2c9b8f9
asmtecno <alex.miranda3@etec.sp.gov.br>
2019-12-06 23:38:50 -0200

    Desafio

--- satanas.js
+++ satanas.js
@@ -1,13 +1,43 @@
 ETEC Professor Horácio Augusto da Silveira
 Avaliação de Design Digital – Segundo Bimestre – Usabilidade e Padrões de Projeto
 
-Nome:__________________________________________________________
+Nome:Alex Sandro Miranda__________________________________________________________
 
 
 
-private void btbanana_Click(object sender, EventArgs e){double BANANA;double CHOPPE; double guaraná; string guaraná; try {BANANA=Convert.ToDouble(txtBANANA.Text);guaraná=Math.Sqrt(BANANA);rguaraná=Convert.ToString(guaraná); txtResultado.Text = rguaraná;} catch(FormatException){ MessageBox.Show("Formatação Errada.");}}
+private void btnbanana_Click(object sender, EventArgs e)
+{
+    double BANANA;
+    double CHOPPE;
+    double guarana;
+    string rguarana;
+    try
+    {
+        BANANA = Convert.ToDouble(txtBANANA.Text);
+        guarana = Math.Sqrt(BANANA);
+        rguarana = Convert.ToString(guarana);
+        rguarana = txtResultado.Text;
+    }
+    catch (FormatException)
+    {
+        MessageBox.Show("Formatação Errada.");
+    }
+}
 
 
-1.	Organize o código acima segundo o que foi explicado nas aulas de técnicas de programação afim de garantir legibilidade e sustentabilidade do código.
+1.	Organize o código acima segundo o que foi explicado nas aulas de técnicas de programação afim de garantir 
+	legibilidade e sustentabilidade do código.
 
-2.	Desenhe uma classe que implemente o código acima como um método. 
+2.	Desenhe uma classe que implemente o código acima como um método.
+
+banana()
+guarana()
+rguarana()
+_________
+incluir+
+somar()
+concatenar()
+
+
+
+ 
